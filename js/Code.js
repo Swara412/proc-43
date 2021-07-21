42a073af4d47e7afe80eb69f3e6ff59c17159a53
Swara412 <80405634+Swara412@users.noreply.github.com>
2021-07-21 20:46:32 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,150)
+    fill("lightblue")
+    text("Hint: it performs a particular tasks !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("B S A D T E A A ", 100,250)
+    fill("lightblue")
+    text("Hint: it stores all the information !!", 100,270)
+}
